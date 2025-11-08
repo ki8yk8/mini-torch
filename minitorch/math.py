b484e58b2eff168feb7a03d245ca096d5b886216
ki8yk8 <k***@gmail.com>
2025-11-08 15:33:00 +0545

    fixes the issue with tuples initialization

--- minitorch/math.py
+++ minitorch/math.py
@@ -2,7 +2,7 @@ from .autograd import Value
 import math
 
 def log(x):
-	result = Value(math.log(x.data), _child=(x), _op="log")
+	result = Value(math.log(x.data), _child=(x,), _op="log")
 
 	def _backward():
 		x.grad += result.grad * 1/x.data
@@ -12,7 +12,7 @@ def log(x):
 	return result
 
 def sin(x):
-	result = Value(math.sin(x.data), _child=(x), _op="sin")
+	result = Value(math.sin(x.data), _child=(x,), _op="sin")
 
 	def _backward():
 		x.grad += result.grad * math.cos(x.data)
@@ -21,7 +21,7 @@ def sin(x):
 	return result
 
 def cos(x):
-	result = Value(math.cos(x.data), _child=(x), _op="cos")
+	result = Value(math.cos(x.data), _child=(x,), _op="cos")
 
 	def _backward():
 		x.grad += result.grad * -math.sin(x.data)
@@ -30,7 +30,7 @@ def cos(x):
 	return result
 
 def tan(x):
-	result = Value(math.tan(x.data), _child=(x), _op="tan")
+	result = Value(math.tan(x.data), _child=(x,), _op="tan")
 
 	def _backward():
 		x.grad += result.grad * 1/(math.cos(x.data)**2)
@@ -39,7 +39,7 @@ def tan(x):
 	return result
 
 def exp(x):
-	result = Value(math.exp(x.data), _child=(x), _op="e")
+	result = Value(math.exp(x.data), _child=(x,), _op="e")
 
 	def _backward():
 		x.grad += result.grad * math.exp(x.data)

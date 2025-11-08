cbddc001b6544d0ca0b0e2938f1f95ec6f25324c
ki8yk8 <k***@gmail.com>
2025-11-08 21:05:22 +0545

    completes and verifies implementation of and gate using autograd

--- test.py
+++ test.py
@@ -1,32 +1,38 @@
 from minitorch.autograd import Value
-from minitorch.activations import ReLU, Sigmoid
 import matplotlib.pyplot as plt
 
 inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
 outputs = [0, 0, 0, 1]
 lr = 0.1
+EPOCHS = 15
 
-w1, w2, b = Value(1.0), Value(-1.0), Value(2.0)
+w1, w2, b = Value(2.0), Value(-1.0), Value(2.0)
 
-model = lambda x1, x2: Sigmoid(w1 * x1 + w2 * x2 + b)
-# model = lambda x1, x2: ReLU(w1 * x1 + w2 * x2 + b)
+model = lambda x1, x2: w1 * x1 + w2 * x2 + b
 
-total_loss = 0
-for [x1, x2], actual in zip(inputs, outputs):
-	predicted = model(x1, x2)
-	loss = (predicted-actual)**2
-	loss.backward()
-	
-	total_loss += loss.data
-	# print(w1.grad, w2.grad, b.grad)
+losses = []
+for i in range(EPOCHS):
+	total_loss = 0
+	for [x1, x2], actual in zip(inputs, outputs):
+		predicted = model(x1, x2)
+		loss = (predicted-actual)**2
+		loss.backward()
+		
+		total_loss += loss.data
+		# print(w1.grad, w2.grad, b.grad)
 
-	w1.data -= lr * w1.grad
-	w2.data -= lr * w2.grad
-	b.data -= lr * b.grad
+		w1.data -= lr * w1.grad
+		w2.data -= lr * w2.grad
+		b.data -= lr * b.grad
 
-	w1.grad = w2.grad = b.grad = 0
+		w1.grad = w2.grad = b.grad = 0
 
-print(w1)
-print(w2)
-print(b)
-print(total_loss)
+	losses.append(total_loss)
+
+print(model(0, 0).data > 0.5)
+print(model(0, 1).data > 0.5)
+print(model(1, 0).data > 0.5)
+print(model(1, 1).data > 0.5)
+
+plt.plot(range(EPOCHS), losses)
+plt.show()

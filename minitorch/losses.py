9b69d9b546c04cfc0c32ea9d50a80ab074cfc5bc
ki8yk8 <k***@gmail.com>
2025-11-11 16:42:55 +0545

    creates Loss class as parent and add tests

--- minitorch/losses.py
+++ minitorch/losses.py
@@ -1,13 +1,26 @@
-def MSE(output, actual):
-	# if list all the output and actual should be list
-	# and the length of list should be equal
-	if isinstance(output, list):
-		if not isinstance(actual, list):
-			raise("Output and Actual must be of type list")
-		if len(output) == len(actual):
-			raise("Length of output, and actual must be equal")
-	else:
-		output, actual = [output], [actual]
+class Loss:
+	def __init__(self):
+		pass
 
-	squared_error = [(o-a)**2 for o, a in zip(output, actual)]
-	return sum(squared_error)/len(squared_error)
+	def sanitize(self, output, actual):
+		# if list all the output and actual should be list
+		# and the length of list should be equal
+		if isinstance(output, list):
+			if not isinstance(actual, list):
+				raise("Output and Actual must be of type list")
+			if not len(output) == len(actual):
+				raise("Length of output, and actual must be equal")
+		else:
+			output, actual = [output], [actual]
+
+		return output, actual
+
+class MSE(Loss):
+	def __init__(self):
+		pass
+
+	def __call__(self, output, actual):
+		output, actual = self.sanitize(output, actual)
+		squared_error = [(o-a)**2 for o, a in zip(output, actual)]
+		
+		return sum(squared_error)/len(squared_error)
--- test.py
+++ test.py
@@ -1,54 +1,7 @@
-from minitorch.nn import Module, Linear, Neuron
-from minitorch.activations import ReLU
-from minitorch.autograd import Value
-from minitorch.optimizers import GD
-import matplotlib.pyplot as plt
+from minitorch.losses import MSE
 
-# xor gate
-inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
-outputs = [0, 1, 1, 0]
-
-LR, EPOCH = 0.01, 4
-
-class Model(Module):
-	def __init__(self):
-		super().__init__()
-		self.linear1 = Linear(in_features=2, out_features=4, bias=True)
-		self.linear2 = Linear(in_features=4, out_features=2, bias=True)
-		self.neuron = Neuron(in_features=2, bias=True)
-		self.bias = Value(0)
-
-	def forward(self, x):
-		op = self.linear1(x)
-		op = ReLU(op)
-		op = self.linear2(op)
-		op = self.neuron(op)
-		op += self.bias
-
-		return op
-
-model = Model()
-optimizer = GD(model.parameters(), lr=LR)
-
-losses = []
-for i in range(EPOCH):
-	total_loss = 0
-	for x, actual in zip(inputs, outputs):
-		predicted = model(x)
-		loss = (predicted-actual)**2
-		loss.backward()
-
-		optimizer.step()
-		optimizer.zero_grad()
-
-		total_loss+=loss.data
-
-	losses.append(total_loss)
-
-plt.plot(range(EPOCH), losses)
-plt.show()
-
-print(f"x1=0, x2=0 => {model([0, 0]).data > 0.5}")
-print(f"x1=0, x2=1 => {model([0, 1]).data > 0.5}")
-print(f"x1=1, x2=0 => {model([1, 0]).data > 0.5}")
-print(f"x1=1, x2=1 => {model([1, 1]).data > 0.5}")
+criteria = MSE()
+print(criteria(1, 0))
+print(criteria([1, 2], [1, 2]))
+print(criteria([1, 2], [1, 3]))
+print(criteria([1, 2], [3, 4]))

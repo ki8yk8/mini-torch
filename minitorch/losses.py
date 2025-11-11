1d77f48e841e3ee7d246c9621ca56f00445b900a
ki8yk8 <k***@gmail.com>
2025-11-11 16:35:57 +0545

    creates MSE loss function

---
+++ minitorch/losses.py
@@ -0,0 +1,13 @@
+def MSE(output, actual):
+	# if list all the output and actual should be list
+	# and the length of list should be equal
+	if isinstance(output, list):
+		if not isinstance(actual, list):
+			raise("Output and Actual must be of type list")
+		if len(output) == len(actual):
+			raise("Length of output, and actual must be equal")
+	else:
+		output, actual = [output], [actual]
+
+	squared_error = [(o-a)**2 for o, a in zip(output, actual)]
+	return sum(squared_error)/len(squared_error)

1d081c8909cc15581a10eddb39cc117d2db92203
ki8yk8 <k***@gmail.com>
2025-11-12 21:22:41 +0545

    create dataset identical to pytorch

--- test.py
+++ test.py
@@ -5,21 +5,38 @@ from minitorch.nn import Linear, Module
 from minitorch.math import exp
 from minitorch.activations import Sigmoid, Softmax
 
-train_df = pd.read_csv("./dataset/mnist_train.csv", header=None)
-test_df = pd.read_csv("./dataset/mnist_test.csv", header=None)
+class MnistDataset:
+	def __init__(self, csv_path, transform=None, target_transform=None):
+		df = pd.read_csv(csv_path, header=None)
+		self.X = df.iloc[:, df.columns !=0].values
+		self.y = df[0].values
+		self.transform = transform
+		self.target_transfrom = target_transform
 
-print(f"Loaded train dataset with size = {len(train_df)}")
-print(f"Loaded test dataset with size = {len(test_df)}\n")
+	def __len__(self):
+		return len(self.y)
+	
+	def __getitem__(self, index):
+		x, y = self.X[index], self.y[index]
 
-# getting the x, y for train and test
-train_x, train_y = train_df.iloc[:, train_df.columns != 0].values, train_df[0].values
-test_x, test_y = test_df.iloc[:, test_df.columns != 0].values, test_df[0].values
+		if self.transform:
+			x = self.transform(x)
 
-# normalizing the train and test x
-def normalize(X):
-	return [[float(x)/255 for x in item] for item in X]
+		if self.target_transfrom:
+			y = self.target_transfrom(y)
 
-train_x, test_x = normalize(train_x), normalize(test_x)
+		return x, y
+
+def normalize_x(X):
+	return [float(x) for x in X]
+
+def normalize_y(y):
+	return int(y)
+
+train_set = MnistDataset("./dataset/mnist_train.csv", transform=normalize_x, target_transform=normalize_y)
+test_set = MnistDataset("./dataset/mnist_test.csv", transform=normalize_x, target_transform=normalize_y)
+
+print(f"Loaded dataset with train = {len(train_set)} and test = {len(test_set)}")
 
 # hyperparameters
 LR = 0.1
@@ -43,4 +60,4 @@ class MNISTClassifier(Module):
 model = MNISTClassifier()
 print(model)
 
-print(test_x[0])
+print(test_set[0])

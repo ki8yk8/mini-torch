84f8736b3668003775b7a6f76b95f1061930628e
ki8yk8 <k***@gmail.com>
2025-11-12 21:03:38 +0545

    fixes the issue with nll and ce to align with pytorch implementation

--- minitorch/losses.py
+++ minitorch/losses.py
@@ -4,7 +4,7 @@ class Loss:
 	def __init__(self):
 		pass
 
-	def sanitize(self, output, actual):
+	def sanitize(self, output, actual, type="regression"):
 		# if list all the output and actual should be list
 		# and the length of list should be equal
 		if isinstance(output, list):
@@ -22,34 +22,32 @@ class MSE(Loss):
 		pass
 
 	def __call__(self, output, actual):
-		output, actual = self.sanitize(output, actual)
+		output, actual = self.sanitize(output, actual, type="regression")
 		squared_error = [(o-a)**2 for o, a in zip(output, actual)]
 		
 		return sum(squared_error)/len(squared_error)
 
+# difference between NLL and CE is that NLL assumes the input is logsoftmax
 class NLL(Loss):
 	def __init__(self):
 		pass
 
-	def __call__(self, output, actual):
-		output, actual = self.sanitize(output, actual)
+	def __call__(self, output, target):
+		if target > len(output) - 1:
+			raise Exception(f"The target {target} is out of bound")
 
-		# NLL = -log(p_y)
-		correct_index = actual.index(1)
-				
-		if not correct_index:
-			raise("There should be one positive target per sample")
-		
-		return -output[correct_index]
+		return -output[target]
 
 class CrossEntropy(Loss):
 	def __init__(self):
 		self.log_softmax = LogSoftmax(stability=True)
 
-	def __call__(self, output, actual):
-		output, actual = self.sanitize(output, actual)
+	def __call__(self, output, target):
+		if target > len(output) -1:
+			raise Exception(f"The target {target} is out of bound of output")
+
 		log_probs = self.log_softmax(output)
 		
-		return NLL()(log_probs, actual)
+		return NLL()(log_probs, target)
 
 

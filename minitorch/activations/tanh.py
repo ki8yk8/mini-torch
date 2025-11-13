bc92f4dbd7beccb98565044cd2a28721bb115598
ki8yk8 <k***@gmail.com>
2025-11-13 20:55:40 +0545

    adds tanh activation function

---
+++ minitorch/activations/tanh.py
@@ -0,0 +1,30 @@
+from ..nn import Module, Value
+from ..helpers import add_indent
+from numbers import Number
+import math
+
+class Tanh(Module):
+	def __init__(self):
+		super().__init__()
+	
+	def get_repr(self, child=0):
+		return add_indent("Tanh()", child)
+
+	def forward(self, x):
+		if isinstance(x, list):
+			return [Tanh()(e) for e in x]
+		elif isinstance(x, Number):
+			x = Value(x)
+		elif isinstance(x, Value):
+			pass
+		else:
+			raise ValueError(f"Tanh expects list, number, Value, got {type(x)}")
+
+		t = math.tanh(x.data)
+		result = Value(t, _child=(x), _op="tanh")
+
+		def _backward():
+			return 1-t*t
+		result._backward = _backward
+
+		return result

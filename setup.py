16764802bb4455ffcf3dbdb781918eeaafb78c6e
ki8yk8 <k***@gmail.com>
2025-11-13 23:09:14 +0545

    install pytest and creates setup for local editable installation of minitorch package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="minitorch",
+	version="0.1.0",
+	author="KI8YK8",
+	description="Implementing minimal Pytorch for Learning",
+	packages=find_packages(),
+	install_requires=[],
+	python_requires=">=3.9",
+)
---
+++ tests/test_loss.py
@@ -0,0 +1,4 @@
+from minitorch.nn import CrossEntropy
+
+def test_cross_entropy():
+	assert 1 == 1

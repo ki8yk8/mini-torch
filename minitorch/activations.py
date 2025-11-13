110d3e57407b52fc16a2688f1ce93c4abf6adfc5
ki8yk8 <k***@gmail.com>
2025-11-13 18:23:03 +0545

    adds leaky relu activation function

--- minitorch/activations.py
+++ minitorch/activations.py
@@ -97,3 +97,27 @@ class LogSoftmax(Module):
 		exponents = [exp(x-max_x) for x in X]
 
 		return [x - max_x - log(sum(exponents)) for x in X]
+
+class LeakyReLU(Module):
+	def __init__(self, negative_slope=0.01):
+		self.negative_slope = 0.01
+
+	def get_repr(self, child):
+		return add_indent(f"LeakyReLU(negative_slope={self.negative_slope})", child)
+	
+	def forward(self, X):
+		if isinstance(X, Value):
+			X = [X]
+		elif isinstance(X, numbers.Number):
+			X = [Value(X)]
+		else:
+			raise ValueError(f"LeakyReLU only accepts type Value, or Number or list of either, got {type(X)}")
+		
+		out = []
+		for x in X:
+			if x > 0:
+				out.append(x)
+			else:
+				out.append(self.negative_slope * x)
+		
+		return out if len(X) > 1 else out[0]

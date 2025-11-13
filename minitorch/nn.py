c8524bf8d204bafc14a7a7adcae844372c8d4f37
ki8yk8 <k***@gmail.com>
2025-11-13 17:32:10 +0545

    adds sequential same as pytorch

--- minitorch/nn.py
+++ minitorch/nn.py
@@ -87,3 +87,25 @@ class Linear(Module):
 			f"Linear(in_features={len(self.neurons[0].weights)}, out_features={len(self.neurons)}, bias={'True' if self.neurons[0].bias else 'False'})",
 			child
 		)
+
+class Sequential(Module):
+	def __init__(self, *layers):
+		super().__init__()
+		self.layers = layers
+
+	def get_repr(self, child=0):
+		representation = add_indent(f"Sequential(\n", child)
+
+		for layer in self.layers:
+			representation += add_indent(layer.get_repr(child+1))
+			representation += "\n"
+
+		representation += add_indent(")", child)
+		return representation
+		
+
+	def forward(self, x):
+		for layer in self.layers:
+			x = layer(x)
+
+		return x
--- test.py
+++ test.py
@@ -1,21 +1,28 @@
-from minitorch.nn import Module, Linear
+from minitorch.nn import Module, Linear, Sequential
 from minitorch.activations import ReLU, Sigmoid, LogSoftmax
 
-class Model(Module):
-	def __init__(self):
-		super().__init__()
-		self.linear = Linear(2, 4)
-		self.relu = ReLU()
-		self.sigmoid = Sigmoid()
-		self.logsoftmax = LogSoftmax(False)
+# class Model(Module):
+# 	def __init__(self):
+# 		super().__init__()
+# 		self.linear = Linear(2, 4)
+# 		self.relu = ReLU()
+# 		self.sigmoid = Sigmoid()
+# 		self.logsoftmax = LogSoftmax(False)
 
-	def forward(self, X):
-		X = self.linear(X)
-		X = self.relu(X)
+# 	def forward(self, X):
+# 		X = self.linear(X)
+# 		X = self.relu(X)
 	
-		return X
+# 		return X
 
-model = Model()
+# model = Model()
+# print(model)
+# output = model([1, 2])
+# output[0].backward()
+
+model = Sequential(
+	Linear(10, 12),
+	Linear(10, 12)
+)
+
 print(model)
-output = model([1, 2])
-output[0].backward()

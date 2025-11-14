0335c518f29635330e411fda870e873e78f42af9
ki8yk8 <k***@gmail.com>
2025-11-14 08:21:26 +0545

    adds test for loss function mse too

--- minitorch/nn/losses/mse.py
+++ minitorch/nn/losses/mse.py
@@ -6,7 +6,7 @@ class MSE(Module):
 
 	def forward(self, output, actual):
 		if not isinstance(output, list) or not isinstance(actual, list):
-			raise ValueError(f"Both output and actual should be of type list")
+			raise ValueError(f"Output and actual should be of type list")
 		
 		if len(output) != len(actual):
 			raise ValueError(f"Mismatch size of output and actual, {len(output)} != {len(actual)}")
--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,4 +1,4 @@
-from minitorch.nn import CrossEntropy
+from minitorch.nn import CrossEntropy, MSE
 from minitorch import Value
 import torch.nn as nn
 import torch
@@ -13,3 +13,14 @@ def test_cross_entropy():
 	torch_logits, torch_target = torch.Tensor(raw_logits), torch.tensor(target)
 
 	assert ce(mini_logits, mini_target).data == torch_ce(torch_logits, torch_target)
+
+def test_mse():
+	torch_mse = nn.MSELoss()
+	mse = MSE()
+
+	raw_logits, target = [1.0, 12.0, 14.0], [2.2, 10.0, 14.1]
+
+	mini_logits, mini_target = [Value(i) for i in raw_logits], [Value(i) for i in target]
+	torch_logits, torch_target = torch.Tensor(raw_logits), torch.Tensor(target)
+
+	assert mse(mini_logits, mini_target).data == torch_mse(torch_logits, torch_target)

bee1be9690a3db01fccc356e33c7c7c11ad8ca11
ki8yk8 <k***@gmail.com>
2025-11-19 20:53:55 +0545

    adds for the upload to pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-	name="minitorch",
-	version="0.1.0",
-	author="KI8YK8",
-	description="Implementing minimal Pytorch for Learning",
-	packages=find_packages(),
-	install_requires=[],
-	python_requires=">=3.9",
+    name="minitorch-ki8yk8",
+    version="1.0.0",
+    author="KI8YK8",
+    description="Implementing minimal Pytorch for Learning",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+                "matplotlib>=3.10.7",
+                "numpy>=2.2.6",
+    ],
+    python_requires=">=3.9",
 )
